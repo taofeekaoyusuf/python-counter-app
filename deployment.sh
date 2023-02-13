a05cf9010164d953906e4cf6ae47da0da6ccb41a
taofeekaoyusuf <dfmtekniks@gmail.com>
2023-02-13 06:03:11 +0100

    Modified: some files

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,10 @@
+# Create a shortcut alias for "kubectl"
 alias k="kubectl"
 
-k 
+# Create Application Deployment
+k create deployment python-counter-app --image=dhackbility/python-counter-app:v1.0.0
+
+# Expose Deployment using the Service
+k expose deployment python-counter-app --name=python-counter-app-service --port=80
+
+# Create Ingress
